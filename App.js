7e8b381dda7de11780b8b19b5d6bc4f9851d319b
mika1407 <koneautomaatio@hotmail.com>
2020-03-31 19:24:18 +0300

    uuid dosent work

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: '0001', text: 'Milk'},
+    {id: '0002', text: 'Eggs'},
+    {id: '0003', text: 'Bread'},
+    {id: '0004', text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
